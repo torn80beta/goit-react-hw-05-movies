556f6043daf820f735e093e4fc8a73048cd0200a
Alex <103459735+torn80beta@users.noreply.github.com>
2023-05-02 03:44:57 +0300

    reviews css

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,6 +1,7 @@
 import { useLocation } from 'react-router-dom';
 import { getMovieReviews } from 'api/api';
 import { useEffect, useState } from 'react';
+import { StyledReviewLi, StyledReviewsUl } from './Reviews.styled';
 
 const Reviews = () => {
   const location = useLocation();
@@ -21,16 +22,16 @@ const Reviews = () => {
   return (
     // console.log(reviews),
     <section>
-      <h2>Reviews</h2>
-      <ul>
+      {/* <h2>Reviews</h2> */}
+      <StyledReviewsUl>
         {reviews.map(review => (
-          <li key={review.id}>
+          <StyledReviewLi key={review.id}>
             <h3>{review.author}</h3>
             <p>{review.created_at}</p>
             <p>{review.content}</p>
-          </li>
+          </StyledReviewLi>
         ))}
-      </ul>
+      </StyledReviewsUl>
     </section>
   );
 };
---
+++ src/components/Reviews/Reviews.styled.jsx
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled';
+
+export const StyledReviewsUl = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 40px;
+`;
+
+export const StyledReviewLi = styled.li`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  padding-bottom: 20px;
+  border-bottom: 1px solid #a0a0a0;
+  text-align: justify;
+`;
